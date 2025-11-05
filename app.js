29139082a3072e331bae7e3fb7989c08a6cf9760
Rygar Schyberg <rygar_schyberg@mines.edu>
2025-11-04 21:15:49 -0700

    Update talking animation

--- app.js
+++ app.js
@@ -27,12 +27,12 @@ async function waitforSpace() {
 async function startSpeech() {
   return new Promise((resolve) => {
     document.addEventListener('keydown', onKeyHandler);
-    function onKeyHandler(e) {
+    async function onKeyHandler(e) {
       if (e.keyCode === 32) {
-        document.removeEventListener('keydown', onKeyHandler);
-        resolve();
         document.querySelector('p').style.visibility = "visible";
         document.querySelector('p').innerHTML = `Pressed space`;
+        await sleep(1000);
+        document.querySelector('p').style.visibility = "hidden";
       }
     }
   });
@@ -44,26 +44,41 @@ async function test() {
     const t0 = performance.now();
     document.querySelector('p').style.visibility = "hidden";
     player.play();
-    document.getElementById("speech").style.color = 'white';
-    document.getElementById("speech").innerHTML = "You: I have a slight cough.";
+    document.getElementById("you").style.color = 'white';
+    document.getElementById("nao").style.color = 'aqua';
+    document.getElementById("you").innerHTML = "You: I have a slight cough.";
 
     await sleep(100);
-    // Press space again when Finished
     startSpeech();
 
     await sleep(3000);
-    document.getElementById("speech").style.color = 'blue';
-    document.getElementById("speech").innerHTML = "Nao: I'm sorry to hear that. Have a cough drop.";
+    document.getElementById("you").style.display = 'none';
+    document.getElementById("nao").innerHTML = "Nao: I'm sorry to hear that. Have a cough drop.";
     player.src({ type: 'video/mp4', src: "videos/talking.mp4" });
     player.play();
 
     await waitforSpace();
-    document.getElementById("speech").style.color = 'white';
-    document.getElementById("speech").innerHTML = "You: Thanks!";
-    await sleep(1000);
+    document.getElementById("nao").style.display = 'none';
+    document.getElementById("you").style.display = "block";
+    document.getElementById("you").innerHTML = "You: Thanks!";
     player.src({ type: 'video/mp4', src: "videos/breathing.mp4" });
     player.play();
-    
+
+    await sleep(2000);
+    document.getElementById("you").style.display = 'none';
+    document.getElementById("nao").style.display = "block";
+    document.getElementById("nao").innerHTML = "Nao: No problem. Do you need anything else?";
+    player.src({ type: 'video/mp4', src: "videos/talking.mp4" });
+    player.play();
+
+    await waitforSpace();
+    document.getElementById("nao").style.display = 'none';
+    document.getElementById("you").style.display = "block";
+    document.getElementById("you").innerHTML = "You: I'm good thanks.";
+    player.src({ type: 'video/mp4', src: "videos/breathing.mp4" });
+    player.play();
+
+
     // const t1 = performance.now();
     // document.querySelector('p').style.visibility = "visible";
     // document.querySelector('p').innerHTML = `Time elapsed: ${t1-t0}`;
